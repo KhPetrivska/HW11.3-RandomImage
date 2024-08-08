08bd43a5806f019c7da75037c65e1f9b9b0e8fd6
Kristina <kristin.petrivska@gmail.com>
2024-08-08 17:32:28 -0600

    remove img num 10

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
 const image = document.querySelector("img");
-const imageNum = Math.floor(Math.random() * 10) + 1;
+const imageNum = Math.floor(Math.random() * 9) + 1;
 
 image.src = `/img/${imageNum}.jpg`;
